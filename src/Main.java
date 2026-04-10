68fde25fa52ac800eb05bb688716419eed87f96b
Anirudh-Krish <anirudhkrishnan@gmail.com>
2026-04-10 15:32:34 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class trainconsistManagement {
+
+    public static void main(String[] args) {
+
+        // Display welcome banner
+        System.out.println("====================================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================================\n");
+
+        // Create a dynamic list to store train bogies
+        List<String> trainConsist = new ArrayList<>();
+
+        // Display initial consist information
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+
+        // Display current train state
+        System.out.println("Current train consist: " + trainConsist);
     }
 }
